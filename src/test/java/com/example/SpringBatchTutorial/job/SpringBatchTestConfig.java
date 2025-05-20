63962c50f35d56e45b509258d5e2a72cb4ab9491
Manjae kim <ei1111@naver.com>
2025-05-20 15:47:33 +0900

    feat: 테스트시 h2 db를 바라도록 수정

--- src/test/java/com/example/SpringBatchTutorial/job/SpringBatchTestConfig.java
+++ src/test/java/com/example/SpringBatchTutorial/job/SpringBatchTestConfig.java
@@ -7,9 +7,10 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@Profile("test")
+@ActiveProfiles("test")
 @SpringBatchTest
 @SpringJUnitConfig(classes = {SpringBatchTutorialApplication.class})
 public abstract class SpringBatchTestConfig {
